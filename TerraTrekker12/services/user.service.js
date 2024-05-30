6521fa5cf1dd8c1f1e93300b4bcf8d92bdc479ef
mansh2000 <waliamanshu17@gmail.com>
2024-05-30 11:48:47 +0530

    added changes

--- TerraTrekker12/services/user.service.js
+++ TerraTrekker12/services/user.service.js
@@ -2,12 +2,18 @@ const userModel = require('../model/user.model');
 const bcrypt = require('bcrypt');
 const ApiError = require('../utils/ApiError');
 const httpStatus = require('http-status');
-const logger=require('../config/logger');
+//const logger=require('../config/logger');
 const saltOrRounds = 10;
 const createUser = async (req) => {
-  logger.info(`userBody data ======>`, req.body);
-  const hashedPassword = await bcrypt.hash(req.body.password, saltOrRounds);
-  return userModel.create({ ...req.body, password: hashedPassword })
+    //logger.info(`userBody data ======>`, req.body);
+    if(await userModel.isEmailTaken(req.body.email)) {
+      throw new ApiError(httpStatus.BAD_REQUEST, 'Email already taken');
+    }
+    if (await userModel.isPhoneNumberTaken(req.body.phoneNo)) {
+      throw new ApiError(httpStatus.BAD_REQUEST, 'PhoneNumber already taken');
+    }
+    const hashedPassword = await bcrypt.hash(req.body.password, saltOrRounds);
+    return userModel.create({ ...req.body, password: hashedPassword })
 };
 const loginUser = async (email, password) => {
   const user = await getUserByEmail(email);
@@ -46,10 +52,34 @@ const paginate = async (userModel, req) => {
     throw new Error('Failed to paginate users: ' + error.message);
   }
 };
+// const changePassword = async (req) => {
+//   try {
+//     const { currentPassword, newPassword, confirmPassword, userType } = req.body;
+//     const userId = req.user;
+//     const user = userType === 'user' ? await User.findOne({ _id: userId }) : await Driver.findOne({ _id: userId });
+//     if (!user) {
+//       throw new ApiError(httpStatus.BAD_REQUEST, 'user not found');
+//     }
+//     if (!(await user.isPasswordMatch(currentPassword))) {
+//       throw new ApiError(httpStatus.BAD_REQUEST, 'Current password does not match');
+//     }
+//     if (newPassword !== confirmPassword) {
+//       throw new ApiError(httpStatus.BAD_REQUEST, 'New password and confirm password do not match');
+//     }
+//     const updatedUser =
+//       userType === 'user'
+//         ? await userService.updateUserById(userId, { password: newPassword })
+//         : await driverService.updateDriverById(userId, { password: newPassword });
+//     return updatedUser;
+//   } catch (err) {
+//     throw new ApiError(httpStatus.BAD_REQUEST, err.message);
+//   }
+// };
 module.exports = {
   createUser,
   loginUser,
   getUserByEmail,
   logout,
-  paginate
+  paginate,
+  //changePassword
 };
