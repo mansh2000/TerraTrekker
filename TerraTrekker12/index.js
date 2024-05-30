8430f200be8a4fc627bce8c6ecdbb5fdf4bd0535
mansh2000 <waliamanshu17@gmail.com>
2024-05-30 12:24:40 +0530

    added changes

--- TerraTrekker12/index.js
+++ TerraTrekker12/index.js
@@ -2,17 +2,20 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const app = require('./app');
+const http = require('http');
+const socketIO = require('socket.io');
+const { server } = require('./services/socket.service');
+
 app.use(bodyParser.json());
 const port = 8000;
 const environment = require('./config/environment');
 
-mongoose.connect(environment.mongodbUri, {
-    useNewUrlParser: true
-}).then(() => {
+mongoose.connect(environment.mongodbUri, 
+).then(() => {
     console.log("database connected");
 }).catch((err) => {
     console.log(err.message);
 })
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`server running on port${port}`);
 })
